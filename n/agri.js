9eb2457a66411eef280af46fd3a70a3d25464ccb
Anuj kumar sharma <141046905+anuj308@users.noreply.github.com>
2024-03-01 00:45:12 +0530

    addde product section

---
+++ n/agri.js
@@ -0,0 +1,194 @@
+console.log("As")
+
+
+const k = ()=>{
+    let a = document.getElementById("product-list")
+    a.innerHTML=`<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Bajra</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Maize (corn)</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Millet</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Rice </div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Soybean</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>`
+}
+const r = ()=>{
+    let a = document.getElementById("product-list")
+    a.innerHTML=`<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Barley</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Gram</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Rapeseed</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Mustard</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Oat</div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Wheat </div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>`
+}
+const z = ()=>{
+    let a = document.getElementById("product-list")
+    a.innerHTML=`<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Pumpkin  </div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Cucumber  </div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>
+<div class="products">
+    <div class="products-main">
+        <div class="products-name"> Bitter Gourd  </div>
+        <div class="products-price right">₹ 70</div>
+    </div>
+    <div class="products-sell">
+        
+        <form action="#">
+            <div class="btn-pro">Buy</div>
+            <div class="btn-pro">Sell</div>
+        </form>
+    </div>
+</div>`
+}
